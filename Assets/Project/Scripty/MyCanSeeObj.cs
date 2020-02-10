fe229664cc15fbe11aae56d06d508ffec3079c76
ly <389113452@qq.com>
2020-02-10 09:37:53 +0800

    提交行为树Demon

---
+++ Assets/Project/Scripty/MyCanSeeObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// 用来判断目标是否在视野内
+/// </summary>
+public class MyCanSeeObj : Conditional
+{
+
+	public Transform[] targets;//判断是否在视野内的目标
+	public float filedOfView = 90;
+	public float ViewDis = 7;
+//
+//	public override TaskStatus OnUpdate()
+//	{
+//		if (targets==null)
+//		{
+//			return TaskStatus.Failure;//任务失败
+//		}
+//
+//		foreach (var target in targets)
+//		{
+//			
+//		}
+//		
+//		
+//	}
+}
---
+++ Assets/Project/Scripty/MySeek.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// 这个任务脚本的作用就是控制游戏物体到达目标位置
+/// </summary>
+public class MySeek : Action
+{
+
+	public Transform target;
+
+	public float speed;
+
+	public float arraiveDis = 0.1f;
+
+	private float dis;
+	/// <summary>
+	/// 当进入到这个任务的时候会，会一直调用这个任务，一直到任务结束，返回一个Running状态
+	/// 调用频率与Unity保持一致
+	// </summary>
+	/// <returns></returns>
+	public override TaskStatus OnUpdate()
+	{
+		if (target==null)
+		{
+			return TaskStatus.Failure;
+		}
+		transform.LookAt(target.position);
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		dis = Vector3.Distance(transform.position, target.position);
+		if (dis<arraiveDis)
+		{
+			return TaskStatus.Success;
+		}
+		return TaskStatus.Running;
+	}
+}
